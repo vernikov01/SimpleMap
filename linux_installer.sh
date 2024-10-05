e7c60cbe0d05ad777a5ea9700004366b736a4584
sp00ked <151371358+vernikov01@users.noreply.github.com>
2024-10-05 23:48:52 +1000

    linux installer bash script (debian based distros)
    install the 'smap' command globally on debian based linux distros rather than have to call the main.py file each time

---
+++ linux_installer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Checking if Nmap has been installed.."
+if ! command -v nmap &> /dev/null; then
+    echo "Nmap is not installed. Installing..."
+    sudo apt install nmap -y
+else
+    echo "Nmap is already installed."
+fi
+
+SHELL_SCRIPT_PATH="/usr/local/bin/smap"
+sudo mkdir /etc/smap
+sudo cp src/main.py /etc/smap
+sudo cp src/basics.py /etc/smap
+sudo cp src/colours.py /etc/smap
+sudo cp src/networkenum.py /etc/smap
+sudo cp src/config.py /etc/smap
+echo "#!/bin/bash" | sudo tee $SHELL_SCRIPT_PATH > /dev/null
+echo "python3 /etc/smap/main.py \"\$@\"" | sudo tee -a $SHELL_SCRIPT_PATH > /dev/null
+sudo chmod +x $SHELL_SCRIPT_PATH
+echo "smap installed successfully. You can now use 'smap' to run the program."
