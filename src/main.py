904e195eaa41e78775e6faf738e2d645f2f5313c
sp00ked <151371358+vernikov01@users.noreply.github.com>
2024-10-06 03:21:36 +1100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,13 +3,15 @@ from networkenum import NmapHandler
 import time
 import subprocess
 import colours as c
+from basics import run_basics
 
 def main():
     print(f"\n{c.CYAN}{c.BOLD}Welcome to Smap! Nmap Made Simple! \n\n {c.RESET}{c.BLUE}Type 'help' for commands.\n{c.RESET}")
     time.sleep(1.25)
-    subprocess.run(['python3', 'src/basics.py'])
+
     
 
 if __name__ == "__main__":
     subprocess.run('clear')
     main()
+    run_basics()
