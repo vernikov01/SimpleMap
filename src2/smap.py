66a58a2a589a9fe0d481f5730a2cbdda1ff66a4b
sp00ked <151371358+vernikov01@users.noreply.github.com>
2024-10-07 04:14:41 +1100

    finally not broke

--- src2/smap.py
+++ src2/smap.py
@@ -2,7 +2,7 @@ import sys
 import time
 import subprocess
 import colours as c
-import commands
+from map import NmapHandler
 from rich import print as rprint
 from rich.panel import Panel
 
@@ -13,9 +13,57 @@ def main():
     time.sleep(1.25)
     print("\n")
 
+import colours as c
+import time 
+from help import helptable
+from map import NmapHandler 
+def nmapscripts():
+    while True:
+        nmap_handler = NmapHandler()
+        command = input(f"{c.BOLD}{c.GREEN}SimpleMap-#:{c.RESET} ").strip()
+
+        if command.lower() in ('exit', 'quit'):
+            time.sleep(0.5)
+            print(f"\n{c.BOLD}{c.CYAN}Exiting Smap Shell.{c.RESET}")
+            time.sleep(1)
+            break
+
+        elif command.lower() == 'help':
+            print(helptable())
+        elif command.startswith("silentvuln "):
+            target = command.split(" ", 1)[1]
+            print("\nScanning... Please wait..\n") #animate this
+            nmap_handler.vulnassess_scan(target)
+
+        elif command.startswith("basic "):
+            target = command.split(" ", 1)[1]
+            print("\nScanning... Please wait..\n")
+            nmap_handler.silent_scan(target)
+
+        elif command.startswith("silentvuln "):
+            target = command.split(" ", 1)[1]
+            print("\nScanning... Please wait..\n")
+            nmap_handler.silent_vulnscan(target)
+
+        elif command.startswith("ping "):
+            target = command.split(" ", 1)[1]
+            print("\n")
+            nmap_handler.execute_command(f"-A T2 -O --script=default --randomize-hosts {target}")
+
+        elif command.startswith("ping "):
+            target = command.split(" ", 1)[1]
+            print("\n")
+            nmap_handler.execute_command(f"{target}")
+
+        else:
+            time.sleep(1)
+            print(f"\n{c.RED}{c.BOLD}Unknown command. Type 'help' for a list of commands.{c.RESET}\n")
+
+
     
 
 if __name__ == "__main__":
     subprocess.run('clear')
     main()
-    commands.NmapHandler()
+    nmapscripts()
+    
